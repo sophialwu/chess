969b1482a423d3b0fb283ac6a08d9763175a4699
Sophia <swoozie14.com>
2016-11-18 20:57:12 -0800

    First commit

---
+++ spec/chess_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Chess do
+  it "has a version number" do
+    expect(Chess::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
