d1d69e3d5cb56232aba5fe7059abbf61938003ff
Sophia <swoozie14.com>
2016-11-21 21:07:38 -0500

    Add requires to multiple classes

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,5 +1,13 @@
 require_relative "chess/version"
 require_relative "chess/player"
+require_relative "chess/piece"
+require_relative "chess/pawn"
+require_relative "chess/rook"
+require_relative "chess/knight"
+require_relative "chess/bishop"
+require_relative "chess/queen"
+require_relative "chess/king"
+require_relative "chess/board"
 
 module Chess
   # Your code goes here...
